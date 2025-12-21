3950e24c2cfb7efb73adbc0f4d596b82f65304d2
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-12-21 14:17:58 -0300

    feat: add function to convert  UNIX to timestamp

--- scripts/fetch_coingecko.py
+++ scripts/fetch_coingecko.py
@@ -1,9 +1,8 @@
 import requests
 from datetime import datetime, timezone
 
-#def unix_to_timestamp(date_str:str) -> int:
-#    return int(
-#        datetime.strptime(date_str, "%Y-%m-%d")
-#        .replace(tzinfo=timezone.utc)
-#        .timestamp()
-#    )
+def to_timestamp(date_str:str) -> int:
+    return int(
+        datetime.strptime(date_str, "%Y-%m-%d")
+        .timestamp()
+    )
