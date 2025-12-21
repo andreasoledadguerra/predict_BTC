5ac58189a4be648c5086d1f9e31247d52643a05b
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-12-20 23:15:36 -0300

    chore: comment function in process to get CoinGecko data

--- scripts/fetch_coingecko.py
+++ scripts/fetch_coingecko.py
@@ -1,2 +1,9 @@
 import requests
 from datetime import datetime, timezone
+
+#def unix_to_timestamp(date_str:str) -> int:
+#    return int(
+#        datetime.strptime(date_str, "%Y-%m-%d")
+#        .replace(tzinfo=timezone.utc)
+#        .timestamp()
+#    )
