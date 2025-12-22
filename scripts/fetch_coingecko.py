95e1e41d5b70b4c6449b7b0bf0408db45708a4ff
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-12-22 12:09:10 -0300

    feat: add headers

--- scripts/fetch_coingecko.py
+++ scripts/fetch_coingecko.py
@@ -1,10 +1,16 @@
+import os
 import requests
+
 from datetime import datetime, timezone
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Function to get UNIX date from CoinGecko 
 def to_timestamp(date_str:str) -> int:
     return int(
         datetime.strptime(date_str, "%Y-%m-%d")
+        .replace(tzinfo=timezone.utc)
         .timestamp()
     )
 
@@ -21,6 +27,9 @@ params = {
     "to": end_ts
 }
 
+headers = {
+    "x-cg-pro-api-key":os.getenv("COINGECKO_API_KEY")
+}
 response = requests.get(url, params=params)
 response.raise_for_status()
 data = response.json()
