7c7a6bbfdcde0419a489ad1636ef53b49a63209b
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-12-23 12:23:49 -0300

    refactor: delete function to_timestamp()

--- scripts/fetch_coingecko.py
+++ scripts/fetch_coingecko.py
@@ -12,16 +12,6 @@ print(f"API Key length: {len(API_KEY) if API_KEY else 0} characters")
 if API_KEY:
     print(f"Key starts with: {API_KEY[:8]}...")
 
-# Function to get UNIX date from CoinGecko 
-def to_timestamp(date_str:str) -> int:
-    dt = datetime.strptime(date_str, "%Y-%m-%d").replace(tzinfo=timezone.utc)
-    timestamp_seconds = int(dt.timestamp())
-    timestamp_ms = timestamp_seconds * 1000
-    print(f"{date_str} -> {timestamp_seconds}s -> {timestamp_ms}ms")
-
-start_ts = to_timestamp("2024-10-01")
-end_ts = to_timestamp("2024-10-07")
-#esta función sirve para recibir fecha inicial y fecha final
 
 #Call the API
 url =  "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range"
