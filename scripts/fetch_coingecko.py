61a7ca4e3dbb2ea02503b9452a82b320ea303927
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-12-23 12:53:43 -0300

    style: add comments  for better readability.

--- scripts/fetch_coingecko.py
+++ scripts/fetch_coingecko.py
@@ -1,34 +1,48 @@
 import os
 import requests
 
+# Standard library imports for date and time handling
 from datetime import datetime, timedelta, timezone
+
+# Third-party library to load environment variables form a .env file
 from dotenv import load_dotenv
 
+# Load variables defined in the .env file into the environment
+# making them accessible via os.getenv()
 load_dotenv()
 
+
+# Retrieve the CoinGecko API key from environment variables
 API_KEY = os.getenv("COINGECKO_API_KEY")
 
+
+# Define the time range for the API request
 end_ts = int(datetime.now(timezone.utc).timestamp())
 start_ts = int((datetime.now(timezone.utc) - timedelta(days=7)).timestamp())
 
-#Call the API
+# CoinGecko endpoint for retrieving price data within a time range
 url =  "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range"
 
+# Query parameterss sent in the request URL
 params = {
     "vs_currency": "usd",
     "from": start_ts,
     "to": end_ts
 }
 
+# HTTP headers, including the demo API key
 headers = {
     "accept": "application/json",
     "x-cg-demo-api-key": API_KEY
 }
+
+# Perform the API request
 response = requests.get(url, params=params, headers=headers)
 
-
+# Parse the JSON response inti a Python dictionary
 data = response.json()
 
 
+# Basic output for inspection and debugging
 print("Keys:", data.keys())
 print("First price entry:", data["prices"][0])
