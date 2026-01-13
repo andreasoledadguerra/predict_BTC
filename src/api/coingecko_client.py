9b165d13f8f4fdf9de35c55f51121bf845ad8adf
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-01-13 14:35:10 -0300

    feat: add get_bitcoin_range() to class CoinGeckoClient

--- src/api/coingecko_client.py
+++ src/api/coingecko_client.py
@@ -13,3 +13,31 @@ class CoinGeckoClient:
         self.session = requests.Session()
         self.session.headers.update({"x-cg-demo-api-key": API_KEY})
 
+    def get_bitcoin_range(
+        self,
+        start_date: str,
+        end_date: str,
+        currency: str = "usd"
+    ) -> Dict:
+        
+        start_ts = self._str_to_timestamp(start_date)
+        end_ts = self._self_str_to_timestamp(end_date)
+
+        url = f"{self.BASE_URL}/coins/bitcoin/market_chart/range"
+
+        params = {
+            "vs_currency": currency,
+            "from": start_ts,
+            "to": end_ts
+        } 
+
+        time.sleep(1) #basic rate limiting
+
+        response = self.session.get(
+            url,
+            params=params,
+            timeout=self.timeout
+        )
+        response.raise_for_status()
+        
+        return response.json()
