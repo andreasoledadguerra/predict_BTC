19d4bc876da4fb393c5dd74c8227447d9aedb3b3
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-01-14 13:04:56 -0300

    refactor: add DateConverter in convert_to_unix()

--- src/utils/date_converter.py
+++ src/utils/date_converter.py
@@ -1,1 +1,16 @@
 from datetime import datetime, timezone
+
+class DateConverter:
+
+    @staticmethod
+    def str_to_timestamp(date_str:str) -> int:
+        dt = datetime.strptime(date_str, "%Y-%m-%d")
+        dt = dt.replace(tzinfo=timezone.utc)
+        return int(dt.timestamp())
+
+    @staticmethod
+    # Convert date string to timestamp Unix
+    def convert_to_unix(start_date:str, end_date: str) -> tuple[int, int]:
+        start_ts = DateConverter.str_to_timestamp(start_date)
+        end_ts =DateConverter.str_to_timestamp(end_date)
+        return start_ts, end_ts
