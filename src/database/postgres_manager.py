e62792184a38a183edf7f1fcee1c5bb6d7942a95
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-01-15 12:56:27 -0300

    feat: initialize class DatabaseManager

--- src/database/postgres_manager.py
+++ src/database/postgres_manager.py
@@ -4,3 +4,26 @@ import logging
 from sqlalchemy import create_engine,text
 from typing import Optimal, Dict, Any
 from contextlib import contextmanager
+
+class DatabaseManager:
+
+    def __init__(
+            self,
+            user: str,
+            password: str,
+            database: str,
+            host: str = "localhost",
+            port: str = "5433"
+    ):
+        
+        self.user = user
+        self.password = password 
+        self.database = database 
+        self.host = host
+        self.port = port 
+        self._engine = None
+        self.logger = logging.getLogger(__name__)
+    
+
+
+
