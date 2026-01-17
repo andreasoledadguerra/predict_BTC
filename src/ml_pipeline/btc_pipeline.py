a5ac7f7648b559946bdf1f43372e166abff8e295
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-01-16 21:32:37 -0300

    feat: add def __init__() to class BTCPredictor

--- src/ml_pipeline/btc_pipeline.py
+++ src/ml_pipeline/btc_pipeline.py
@@ -3,3 +3,12 @@ import pandas as pd
 import logging
 from sklearn.linear_model import LinearRegression
 from typing import Tuple, Optional, List
+
+class BTCPredictor:
+
+    def __init__(self, model: Optional[LinearRegression] = None):
+
+        self.model = model or LinearRegression()
+        self.logger = logging.getLogger(__name__)
+        self.is_trained = False
+        self.training_size = 0 
