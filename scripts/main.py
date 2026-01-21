f82fea789429ca8e3c3c50de12cedab7595e6fd7
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-01-21 10:27:47 -0300

    chore: retrieve Postgres environment variables

--- scripts/main.py
+++ scripts/main.py
@@ -12,3 +12,9 @@ from pipeline.btc_pipeline import BTCDataPipeline
 
 # Load variables defined in the .env file into the environment (delete)
 load_dotenv()
+
+# Retrieve the Postgres environment variables (delete)
+POSTGRES_USER= os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD= os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB= os.getenv("POSTGRES_DB")
+POSTGRES_PORT= os.getenv("POSTGRES_PORT")
