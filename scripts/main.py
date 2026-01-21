a4e9bbc3275a700f65e0492ecf75411ed78f0efd
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-01-21 10:45:32 -0300

    feat: add function initialize_components() from the pipeline

--- scripts/main.py
+++ scripts/main.py
@@ -21,3 +21,29 @@ POSTGRES_PORT= os.getenv("POSTGRES_PORT")
 
 # Retrieve the CoinGecko API key from environment variables(delete)
 API_KEY = os.getenv("COINGECKO_API_KEY")
+
+
+# ====================================== SETTING =================================================
+def initialize_components(env_vars):
+
+    api_client = CoinGeckoClient(api_key=env_vars["COINGECKO_API_KEY"])
+
+    db_manager = DatabaseManager(
+        user=env_vars["POSTGRES_USER"],
+        password=env_vars["POSTGRES_PASSWORD"],
+        database=env_vars["POSTGRES_DB"],
+        port=env_vars["POSTGRES_PORT"]
+    )
+
+    predictor = BTCPredictor()
+
+    pipeline = BTCDataPipeline(api_client,db_manager, predictor)
+
+    return pipeline, db_manager
+
+
+
+# ====================================== STAGE 1 ================================================
+
+
+
