06f2cd3031c2a99244f84696ca8a8290b93a0ef9
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-01-21 10:19:04 -0300

    feat: integrate BTC data pipeline components

--- scripts/main.py
+++ scripts/main.py
@@ -4,3 +4,9 @@ import logging
 from datetime import datetime
 from dotenv import load_dotenv
 
+
+from api.coingecko_client import CoinGeckoClient
+from database.postgres_manager import DatabaseManager
+from ml.btc_predictor import BTCPredictor
+from pipeline.btc_pipeline import BTCDataPipeline
+
