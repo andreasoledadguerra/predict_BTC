be655cb1454bbd577bb65ab374b401dce1892739
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-01-23 10:17:15 -0300

    fix: update relaitive imports

--- src/main.py
+++ src/main.py
@@ -5,10 +5,10 @@ from datetime import datetime
 from dotenv import load_dotenv
 
 
-from api.coingecko_client import CoinGeckoClient
-from database.postgres_manager import DatabaseManager
-from ml.btc_predictor import BTCPredictor
-from pipeline.btc_pipeline import BTCDataPipeline
+from .api.coingecko_client import CoinGeckoClient
+from .database.postgres_manager import DatabaseManager
+from .ml.btc_predictor import BTCPredictor
+from .pipeline.btc_pipeline import BTCDataPipeline
 
 # Load variables defined in the .env file into the environment (delete)
 load_dotenv()
