daeb01fab9cb7d5b6ee583059e3c74531faca9e4
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-01-23 09:53:41 -0300

    fix: update relative imports

--- src/pipeline/btc_pipeline.py
+++ src/pipeline/btc_pipeline.py
@@ -5,9 +5,9 @@ from typing import Dict, Any, Optional, Tuple
 import pandas as pd
 
 
-from api.coingecko_client import CoinGeckoClient
-from database.postgres_manager import DatabaseManager
-from ml.btc_predictor import BTCPredictor
+from ..api.coingecko_client import CoinGeckoClient
+from ..database.postgres_manager import DatabaseManager
+from ..ml.btc_predictor import BTCPredictor
 
 
 class BTCDataPipeline:
