4bd92932958fe602a8115a3123386c71cdd1d8f5
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-01-25 11:32:50 -0300

    feat: add classes PostgresSettings and CoinGeckoSettings

--- src/config/settings.py
+++ src/config/settings.py
@@ -3,3 +3,14 @@ from dataclasses import dataclass
 from dotenv import load_dotenv
 
 load_dotenv()
+
+@dataclass(frozen=True)
+class PostgresSettings:
+    user: str
+    password: str
+    database: str
+    port : int
+
+@dataclass(frozen=True)
+class CoinGeckoSettings:
+    API_KEY:str
