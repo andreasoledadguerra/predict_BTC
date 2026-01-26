2dcbdff8c421ede595742c3cc9842fa4343c3c83
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-01-26 10:29:07 -0300

    style: fix indentation

--- src/api/coingecko_client.py
+++ src/api/coingecko_client.py
@@ -22,73 +22,55 @@ class CoinGeckoClient:
         self.session.headers.update({"x-cg-demo-api-key": API_KEY})
 
 
-        def build_request_config(self,start_ts: int, end_ts: int) -> dict:
-            
-            config = {
-                "url":  f"{self.BASE_URL}/coins/bitcoin/market_chart/range",
-                "params": {      
-                    "vs_currency": "usd",
-                    "from": start_ts,
-                    "to": end_ts
-                }
+    def build_request_config(self,start_ts: int, end_ts: int) -> dict:
+        
+        config = {
+            "url":  f"{self.BASE_URL}/coins/bitcoin/market_chart/range",
+            "params": {      
+                "vs_currency": "usd",
+                "from": start_ts,
+                "to": end_ts
             }
-        
-            return config
-
-
-        def parse_price_data(self, data:dict) -> pd.DataFrame:
-
-            if "prices" not in data:
-                raise ValueError ("La respuesta no contiene 'prices'")
-
-            df = pd.DataFrame(data["prices"], columns=["timestamp_ms", "price_usd"])
-
-            # Transformar timestamp a fecha (borrar)
-            df["date"] = pd.to_datetime(df["timestamp_ms"], unit="ms").dt.date
-
-            # Agregar columna de activo
-            df["asset"] = "BTC"
-
-            # Reordenar columnas
-            df = df[["date", "price_usd", "asset"]]
-
-            return df
+        }
     
+        return config
+    def parse_price_data(self, data:dict) -> pd.DataFrame:
+        if "prices" not in data:
+            raise ValueError ("La respuesta no contiene 'prices'")
+        df = pd.DataFrame(data["prices"], columns=["timestamp_ms", "price_usd"])
+        # Transformar timestamp a fecha (borrar)
+        df["date"] = pd.to_datetime(df["timestamp_ms"], unit="ms").dt.date
+        # Agregar columna de activo
+        df["asset"] = "BTC"
+        # Reordenar columnas
+        df = df[["date", "price_usd", "asset"]]
+        return df
 
-        def str_to_timestamp(self, date_str:str) -> int:
-            dt = datetime.strptime(date_str, "%Y-%m-%d")
-            dt = dt.replace(tzinfo=timezone.utc)
-            return int(dt.timestamp())
-
-        
-        def fetch_bitcoin_prices(self, start_date: str, end_date: str) -> pd.DataFrame:
-
-            self.logger.info(f"Getting data from {start_date} to {end_date}...")
-
-            start_ts, end_ts = DateConverter.convert_to_unix(start_date, end_date)
-
-            request_config = self._build_request_config(start_ts, end_ts)
-
-            time.sleep(1)
+    def str_to_timestamp(self, date_str:str) -> int:
+        dt = datetime.strptime(date_str, "%Y-%m-%d")
+        dt = dt.replace(tzinfo=timezone.utc)
+        return int(dt.timestamp())
     
-            response = self.session.get(
-                request_config["url"],
-                params=request_config["params"],
-                timeout=self.timeout
-                
-            )
-            response.raise_for_status()
+    def fetch_bitcoin_prices(self, start_date: str, end_date: str) -> pd.DataFrame:
+        self.logger.info(f"Getting data from {start_date} to {end_date}...")
+        start_ts, end_ts = DateConverter.convert_to_unix(start_date, end_date)
+        request_config = self._build_request_config(start_ts, end_ts)
+        time.sleep(1)
 
-            raw_data = response.json()
-
-            df = self._parse_price_data(raw_data)
-
-            self.logger.info(f" {len(df)} obtained records")
-            return df
-        
-
-        def close(self):
-            self.session.close()
-        
-        def __exit__(self, exc_tye, exc_val, exc_tb):
-            self.close()
+        response = self.session.get(
+            request_config["url"],
+            params=request_config["params"],
+            timeout=self.timeout
+            
+        )
+        response.raise_for_status()
+        raw_data = response.json()
+        df = self._parse_price_data(raw_data)
+        self.logger.info(f" {len(df)} obtained records")
+        return df
+    
+    def close(self):
+        self.session.close()
+    
+    def __exit__(self, exc_tye, exc_val, exc_tb):
+        self.close()
