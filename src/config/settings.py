83ae5099c0d8ff22d3f6e91182911c981cad6b74
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-02-01 16:22:00 -0300

    feat: add methid get_connection_string() to class PostgresSettings

--- src/config/settings.py
+++ src/config/settings.py
@@ -9,7 +9,15 @@ class PostgresSettings:
     user: str
     password: str
     database: str
-    port : int
+    host: str = "localhost"
+    port: int = 5433
+
+    def get_connection_string(self):
+        connection_string = (
+            f"postgresql+psycopg2://{self.user}:{self.password}"
+            f"@{self.host}:{self.port}/{self.database}"
+        )
+        return connection_string
 
 @dataclass(frozen=True)
 class CoinGeckoSettings:
@@ -20,7 +28,9 @@ def get_postgres_settings() -> PostgresSettings:
         user=os.getenv("POSTGRES_USER"),
         password=os.getenv("POSTGRES_PASSWORD"),
         database=os.getenv("POSTGRES_DB"),
+        host=os.getenv("POSTGRES_HOST", "localhost"),
         port=int(os.getenv("POSTGRES_PORT", 5433)),
+
     )
 
 def get_coingecko_settings() -> CoinGeckoSettings:
