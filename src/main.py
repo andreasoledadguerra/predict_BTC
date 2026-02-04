96272b2f2054b6885bb77bd9f05ed12aa5eaf514
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-02-04 11:44:34 -0300

    refactor: switch relative imports to absolute imports

--- src/main.py
+++ src/main.py
@@ -4,12 +4,11 @@ import logging
 from datetime import datetime
 from dotenv import load_dotenv
 
-from .config.settings import get_postgres_settings, get_coingecko_settings
-
-from .api.coingecko_client import CoinGeckoClient
-from .database.postgres_manager import DatabaseManager
-from .ml.btc_predictor import BTCPredictor
-from .pipeline.btc_pipeline import BTCDataPipeline
+from src.config.settings import get_postgres_settings, get_coingecko_settings
+from src.api.coingecko_client import CoinGeckoClient
+from src.database.postgres_manager import DatabaseManager
+from src.ml.btc_predictor import BTCPredictor
+from src.pipeline.btc_pipeline import BTCDataPipeline
 from src.config.logs import configure_logging
 
 logger = logging.getLogger(__name__)
