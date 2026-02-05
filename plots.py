892c4198f8f0c08aa960a6301e9d1382ca344a45
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-02-05 13:19:29 -0300

    feat:  add structure for BTC prediction visualization

---
+++ plots.py
@@ -0,0 +1,12 @@
+# plot.py
+
+"""
+Visualization script for Bitcoin price prediction models.
+
+This module will contain functions to:
+- Load prediction model results
+- Generate comparison charts
+- Display BTC price forecasts
+"""
+
+# TODO: Implement visualization functions
