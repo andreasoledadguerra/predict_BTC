59deb01b3e1f69c858423f1da9713c5a9a79b37c
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-02-22 16:11:19 -0300

    fix: fix data leakage in feature engineering by using shift(1)

--- src/ml/btc_predictor.py
+++ src/ml/btc_predictor.py
@@ -32,22 +32,26 @@ class BTCPredictor:
             df[f'lag_{lag}'] = df['price'].shift(lag)
 
         for w in self.windows:
-            df[f'rolling_mean_{w}'] = df['price'].rolling(window=w, min_periods=1).mean()
-            df[f'rolling_std_{w}'] = df['price'].rolling(window=w, min_periods=2).std()
+            df[f'rolling_mean_{w}'] = df['price'].shift(1).rolling(window=w, min_periods=1).mean()
+            df[f'rolling_std_{w}'] = df['price'].shift(1).rolling(window=w, min_periods=2).std()
+
+
+            #df[f'rolling_mean_{w}'] = df['price'].rolling(window=w, min_periods=1).mean()
+            #df[f'rolling_std_{w}'] = df['price'].rolling(window=w, min_periods=2).std()
 
         #Add tendency's features
         # 1. Momentum (cambio porcentual)
-        df['price_change_1d'] = df['price'].pct_change(1)
-        df['price_change_7d'] = df['price'].pct_change(7)
-        # 2. Ratio precio actual vs media
-        df['price_to_ma7'] = df['price'] / df['rolling_mean_7']
-        df['price_to_ma14'] = df['price'] / df['rolling_mean_14']
-        # 3. Diferencias
-        df['diff_1d'] = df['price'].diff(1)
-        df['diff_7d'] = df['price'].diff(7)
+        #df['price_change_1d'] = df['price'].pct_change(1)
+        #df['price_change_7d'] = df['price'].pct_change(7)
+        ## 2. Ratio precio actual vs media
+        #df['price_to_ma7'] = df['price'] / df['rolling_mean_7']
+        #df['price_to_ma14'] = df['price'] / df['rolling_mean_14']
+        ## 3. Diferencias
+        #df['diff_1d'] = df['price'].diff(1)
+        #df['diff_7d'] = df['price'].diff(7)
     
         # Volatilidad
-        df['volatility_7d'] = df['rolling_std_7'] / df['rolling_mean_7']
+        #df['volatility_7d'] = df['rolling_std_7'] / df['rolling_mean_7']
 
         df.dropna(inplace=True)
 
