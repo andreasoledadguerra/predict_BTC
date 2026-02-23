42e5fc3627981b074d6133f8aaadd183dfe9312e
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-02-23 09:33:25 -0300

    style: delete commented lines

--- src/ml/btc_predictor.py
+++ src/ml/btc_predictor.py
@@ -35,24 +35,6 @@ class BTCPredictor:
             df[f'rolling_mean_{w}'] = df['price'].shift(1).rolling(window=w, min_periods=1).mean()
             df[f'rolling_std_{w}'] = df['price'].shift(1).rolling(window=w, min_periods=2).std()
 
-
-            #df[f'rolling_mean_{w}'] = df['price'].rolling(window=w, min_periods=1).mean()
-            #df[f'rolling_std_{w}'] = df['price'].rolling(window=w, min_periods=2).std()
-
-        #Add tendency's features
-        # 1. Momentum (cambio porcentual)
-        #df['price_change_1d'] = df['price'].pct_change(1)
-        #df['price_change_7d'] = df['price'].pct_change(7)
-        ## 2. Ratio precio actual vs media
-        #df['price_to_ma7'] = df['price'] / df['rolling_mean_7']
-        #df['price_to_ma14'] = df['price'] / df['rolling_mean_14']
-        ## 3. Diferencias
-        #df['diff_1d'] = df['price'].diff(1)
-        #df['diff_7d'] = df['price'].diff(7)
-    
-        # Volatilidad
-        #df['volatility_7d'] = df['rolling_std_7'] / df['rolling_mean_7']
-
         df.dropna(inplace=True)
 
         return df
@@ -107,19 +89,6 @@ class BTCPredictor:
         self.logger.info(f"Model trained. R² = {r2:.4f}")
 
 
-    #def train(self, X: np.ndarray, y: np.ndarray) -> None:
-    #    
-    #    X_scaled = self.scaler.fit_transform(X)
-    #    self.model.fit(X_scaled, y)
-    #    self.is_trained = True
-#
-    #    n_features = X.shape[1]
-    #    self.feature_names = [f'feature_{i}' for i in range(n_features)]
-#
-    #    score = self.model.score(X_scaled, y)
-    #    self.logger.info(f"Model trained. R² = {score:.4f}")
-
-
     def predict_future(self, n_days: int, last_prices=None) -> np.ndarray:
     
         if last_prices is not None:
@@ -167,55 +136,3 @@ class BTCPredictor:
 
         return np.array(predictions)
 
-# =======================================================ORIGINAL===============================================================
-#import numpy as np
-#import pandas as pd
-#import logging
-#from sklearn.linear_model import LinearRegression
-#from typing import Tuple, Optional, List
-#
-#
-#class BTCPredictor:
-#
-#    def __init__(self, model: Optional[LinearRegression] = None):
-#
-#        self.model = model or LinearRegression()
-#        self.logger = logging.getLogger(__name__)
-#        self.is_trained = False 
-#        self.training_size = 0 
-#        #self.predict = predict
-#    
-#
-#    def prepare_training_data(self, df: pd.DataFrame) -> Tuple[np.ndarray, np.ndarray]:
-#
-#        prices = df["price_usd"].values
-#        X = np.arange(len(prices)).reshape(-1, 1)
-#        y = prices
-#
-#        self.training_size = len(X)
-#        self.logger.info(f"Prepared data: {self.training_size} samples")
-#        return X, y
-#
-#
-#    def train(self, X: np.ndarray, y: np.ndarray) -> None:
-#
-#        self.logger.info(f" Training model with {len(X)} datos...")
-#        self.model.fit(X, y)
-#        self.is_trained = True
-#
-#        # “Returns a number that measures how well the model predicts.”
-#        score = self.model.score(X, y)
-#        self.logger.info(f"Trained model. R² score: {score:.4f}")
-#
-#
-#    def predict_future(self, n_days:int) -> np.ndarray:
-#        x_future = np.arange(
-#            self.training_size,
-#            self.training_size + n_days
-#        ).reshape(-1,1)
-#        predictions = self.model.predict(x_future)
-#        self.logger.info(f"{n_days} predicciones generadas")
-#        return predictions
-#        
-#        
-#
