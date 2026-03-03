55886fcd0a98bce2665c0e37a9fc6d0aa52ebc34
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-03-03 10:36:24 -0300

    feat: read and convert historical data  from column 'Open time'.

--- tools/historical_BTC.py
+++ tools/historical_BTC.py
@@ -3,10 +3,10 @@
 import pandas as pd
 import sqlite3 
 
-from dotenv import load_dotenv
-from src.api.coingecko_client import CoinGeckoClient
-from src.database.postgres_manager import DatabaseManager
-from src.ml.btc_predictor import BTCPredictor
-from src.pipeline.btc_pipeline import BTCDataPipeline
-from src.config.settings import get_postgres_settings, get_coingecko_settings
+# Read .csv file
+df= pd.read_csv('BTCUSD_1d_Binance.csv')
 
+# Convert column to datetime
+df['Open time']=pd.to_datetime(df['Open time'])
+
+#
