4eb175fe9debfa4f40afea3a32bb3511ea2b2313
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-03-03 10:50:21 -0300

    feat: define range date and create mask to extract data

--- tools/historical_BTC.py
+++ tools/historical_BTC.py
@@ -9,4 +9,13 @@ df= pd.read_csv('BTCUSD_1d_Binance.csv')
 # Convert column to datetime
 df['Open time']=pd.to_datetime(df['Open time'])
 
-#
+# Define range date
+start_date = '2025-01-01'
+end_date = '2025-09-30'
+
+# Create mask using the column date
+mask = (df['Open time'] >= start_date) & (df['Open time'] <= end_date)
+df_filtered = df.loc[mask]
+
+#print(f"Registros del rango: {len(df_filtered)}")
+
