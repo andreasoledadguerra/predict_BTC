fa1a6d9cc64bd103bfcffe778ee14cc31e12ceae
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-03-06 13:02:45 -0300

    chore: import class BTCPredictor and libraries

---
+++ src/ml/optimization_ridge.py
@@ -0,0 +1,5 @@
+import numpy as np
+from sklearn.model_selection import TimeSeriesSplit
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import Ridge
+from src.ml.btc_predictor import     BTCPredictor
